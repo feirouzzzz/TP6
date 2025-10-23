45ea3122a208d82752f0ef672b1f4136d5787cdc
feirouzzzz <feirouzelmouhtadi@gmail.com>
2025-10-23 13:19:01 +0100

    first

---
+++ src/main/java/com/example/demo/entities/Student.java
@@ -0,0 +1,52 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nom;
+    private String prenom;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance;
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+}
